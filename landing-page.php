5d9ae23fa878ce125d93794a8fa4d56455146e16
Andreirl032 <andrei.ramos.lopes@gmail.com>
2024-05-02 00:37:15 -0300

    first commit

---
+++ landing-page.php
@@ -0,0 +1,24 @@
+<?php
+echo "oi mundoo"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="landing-page.css" /> 
+    <title>Document</title>
+</head>
+<body>
+    <p>oiee</p>
+    <div class="div-cadastro">
+        <h1>Cadastro</h1>
+        <div class="div-inputs">
+        <input type="text" placeholder="Nome completo"/>
+        <input type="text" placeholder="E-mail"/>
+        <input type="text" placeholder="Contato"/>
+        </div>
+    </div>
+</body>
+</html>
